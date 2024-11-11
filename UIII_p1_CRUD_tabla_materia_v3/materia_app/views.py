f53cdbd48c7755dfe537f30811d2046e4c3f9244
danielaj22 <a.22308051281069@cbtis128.edu.mx>
2024-11-11 14:04:28 -0600

    Add files via upload

---
+++ UIII_p1_CRUD_tabla_materia_v3/materia_app/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from .models import Materia
+
+def inicio_vista(request):
+    lasmaterias=Materia.objects.all()
+# Create your views here.
+    return render(request,'gestionarMaterias.html',{"mismaterias":lasmaterias})
+
+
+
+def registrarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    creditos=request.POST["numcreditos"]
+
+
+    registrarMateria=Materia.objects.create(codigo=codigo,nombre=nombre,creditos=creditos)
+    
+
+    
+    return redirect("/")
+
+def seleccionarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    return render(request,"editarmateria.html",{"mismaterias":materia})
+
+
+def editarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    creditos=request.POST["numcreditos"]
+
+
+    materia=Materia.objects.get(codigo=codigo)
+    materia.nombre=nombre
+    materia.creditos=creditos
+    materia.save()
+    return redirect("/")
+
+
+def borrarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    materia.delete()
+
+    return redirect("/")
